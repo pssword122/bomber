c7731839bda3eca60f9a013c2116e96399f32909
pssword122 <81179254+pssword122@users.noreply.github.com>
2022-02-10 22:55:03 +0300

    Update Bomber.py

--- Bomber.py
+++ Bomber.py
@@ -1,1 +1,8 @@
-https://github.com/pssword122/pssword122/blob/main/BotSms.py
+import requests, fake_useragent
+
+user = fake_useragent.UserAgent().random
+headers = {'user_agent' : user}
+NUMBER = input('Введите номер телефона: (Без +)')
+
+try:
+response = requests.post('https://shop.vsk.ru/ajax/auth/postSms/' , headers=headers, data={'phone' : })
